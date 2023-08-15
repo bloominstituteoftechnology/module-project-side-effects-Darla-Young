05d840442bff0f42163e560155a1097d02e6a37a
Darla Young <89855627+Darla-Young@users.noreply.github.com>
2023-08-15 15:32:36 -0500

    basically complete

--- frontend/components/App/App.js
+++ frontend/components/App/App.js
@@ -4,34 +4,32 @@
   Look at each file to see what props need to be passed!
 */
 
-// Import the state hook
-import React from 'react';
-// Import the Posts (plural!) and SearchBar components, since they are used inside App component
-// Import the dummyData
+import React, {useState} from 'react';
+import Posts from '../Posts/Posts.js';
+import SearchBar from '../SearchBar/SearchBar.js';
+import dummyData from '../../data/dummy-data.js';
 
 const App = () => {
-  // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
-  // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
-  // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  const [posts, setPosts] = useState(dummyData);
+  // const [searchTerm, setSearchTerm] = useState('');
 
+  // increases the number of likes by one of the post with a given id.
   const likePost = postId => {
-    /*
-      This function serves the purpose of increasing the number of likes by one, of the post with a given id.
-
-      The state of the app lives at the top of the React tree, but it wouldn't be fair for nested components not to be able to change state!
-      This function is passed down to nested components through props, allowing them to increase the number of likes of a given post.
-
-      Hints: Invoke `setPosts` and pass as the new state the invocation of `posts.map`.
-      The callback passed into `map` performs the following logic:
-        - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
-        - otherwise just return the post object unchanged.
-     */
+    const postArr = [];
+      posts.map(
+        post => {
+          postArr.push(
+            postId === post.id ? {...post, likes: post.likes + 1} : post
+          );
+        }
+      );
+    setPosts(postArr);
   };
 
   return (
     <div className='App'>
-      {/* Add SearchBar and Posts here to render them */}
-      {/* Check the implementation of each component, to see what props they require, if any! */}
+      <SearchBar />
+      <Posts posts={posts} likePost={likePost}/>
     </div>
   );
 };
--- frontend/data/dummy-data.js
+++ frontend/data/dummy-data.js
@@ -1,9 +1,21 @@
+import Axios from 'axios';
+
+let thumbnailUrl = '';
+let imageURL = '';
+Axios.get('https://randomuser.me/api')
+  .then(response => {
+    thumbnailUrl = response.data.results[0].picture.thumbnail;
+    imageURL = response.data.results[0].picture.large;
+  });
+
 const dummyData = [
   {
     id: 1,
     username: "philzcoffee",
-    thumbnailUrl: 'https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg',
-    imageUrl: 'https://images.unsplash.com/photo-1477763858572-cda7deaa9bc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1876&q=80',
+    thumbnailUrl: thumbnailUrl,
+    // thumbnailUrl: 'https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg',
+    imageUrl: imageURL,
+    // 'https://images.unsplash.com/photo-1477763858572-cda7deaa9bc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1876&q=80',
     likes: 400,
     timestamp: "July 17th 2017, 12:42:40 pm",
     comments: [
@@ -27,8 +39,10 @@ const dummyData = [
   {
     id: 2,
     username: "twitch",
-    thumbnailUrl: 'https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg',
-    imageUrl: 'https://images.unsplash.com/photo-1566577134770-3d85bb3a9cc4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3750&q=80',
+    thumbnailUrl: thumbnailUrl,
+    // thumbnailUrl: 'https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg',
+    imageUrl: imageURL,
+    // 'https://images.unsplash.com/photo-1566577134770-3d85bb3a9cc4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3750&q=80',
     likes: 4307,
     timestamp: "July 15th 2017, 03:12:09 pm",
     comments: [
